2bc883e1c34eb51ef35b2a556a45095237e467e1
Ricardo Asin <ricardoasinr@gmail.com>
2024-01-11 01:19:33 -0400

    Stream funcionando

--- lib/models/door.dart
+++ lib/models/door.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: avoid_print
+
+import 'dart:math';
+
 class Door {
   final bool statusDoor;
   final DateTime lastUpdate;
@@ -18,4 +22,42 @@ Map<String, dynamic> mapCreateDoor({
   });
 
   return firestoreData;
+}
+
+Future<void> createData(final db) async {
+  await db.collection('doors').add({
+    'statusDoor': true,
+    'lastUpdate': DateTime.now(),
+  });
+}
+
+Future<void> createData2(final db) async {
+  await db.collection('doors').add(mapCreateDoor(
+        statusDoor: true,
+        lastUpdate: DateTime.now(),
+      ));
+}
+
+Future<void> openDoor(final db) async {
+  await db
+      .collection('doors')
+      .doc("OCJtX1n28YrORFdmaoTD")
+      .set(mapCreateDoor(
+        statusDoor: true,
+        lastUpdate: DateTime.now(),
+      ))
+      .then((value) => print("Successfully! Door is open"),
+          onError: (e) => print("Error: $e"));
+}
+
+Future<void> closeDoor(final db) async {
+  await db
+      .collection('doors')
+      .doc("OCJtX1n28YrORFdmaoTD")
+      .set(mapCreateDoor(
+        statusDoor: false,
+        lastUpdate: DateTime.now(),
+      ))
+      .then((value) => print("Successfully! Door is closed"),
+          onError: (e) => print("Error: $e"));
 }
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -14,68 +14,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // bool _openDoor = false;
   final db = FirebaseFirestore.instance;
-
-  Future<void> createData() async {
-    await db.collection('doors').add({
-      'statusDoor': true,
-      'lastUpdate': DateTime.now(),
-    });
-  }
-
-  Future<void> createData2() async {
-    await db.collection('doors').add(mapCreateDoor(
-          statusDoor: true,
-          lastUpdate: DateTime.now(),
-        ));
-  }
-
-  // void getStatusDoor() async {
-  //   var response =
-  //       await db.collection('doors').doc("OCJtX1n28YrORFdmaoTD").get();
-
-  //   bool result = response.data()!["statusDoor"];
-
-  //   setState(() {
-  //     _openDoor = !result;
-  //   });
-  // }
-
-  Future<void> openDoor() async {
-    await db
-        .collection('doors')
-        .doc("OCJtX1n28YrORFdmaoTD")
-        .set(mapCreateDoor(
-          statusDoor: true,
-          lastUpdate: DateTime.now(),
-        ))
-        .then((value) => log("Successfully! Door is open"),
-            onError: (e) => log("Error: $e"));
-    ;
-  }
-
-  Future<void> closeDoor() async {
-    await db
-        .collection('doors')
-        .doc("OCJtX1n28YrORFdmaoTD")
-        .set(mapCreateDoor(
-          statusDoor: false,
-          lastUpdate: DateTime.now(),
-        ))
-        .then((value) => log("Successfully! Door is closed"),
-            onError: (e) => log("Error: $e"));
-  }
 
   @override
   void initState() {
-    // TODO: implement initState
-
     super.initState();
-    // getStatusDoor();
   }
-
-  //OCJtX1n28YrORFdmaoTD
 
   @override
   Widget build(BuildContext context) {
@@ -104,7 +48,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   Padding(
                     padding: const EdgeInsets.all(30.0),
                     child: Icon(
-                      // document!["statusDoor"]
                       document?["statusDoor"] ?? false
                           ? Icons.door_sliding_outlined
                           : Icons.door_back_door,
@@ -116,13 +59,11 @@ class _MyHomePageState extends State<MyHomePage> {
                       onPressed: () async {
                         if ((document?["statusDoor"] ?? false) == true) {
                           setState(() {
-                            // _openDoor = false;
-                            closeDoor();
+                            closeDoor(db);
                           });
                         } else {
                           setState(() {
-                            // _openDoor = true;
-                            openDoor();
+                            openDoor(db);
                           });
                         }
                       },
